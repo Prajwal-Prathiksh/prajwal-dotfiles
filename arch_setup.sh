cfebb83015e08361662e2af2f5d73e5f70c504c5
Prajwal-Prathiksh <prajwal.prathiksh@gmail.com>
2025-02-17 14:31:26 +0000

    Add Zsh configuration options and custom setup scripts to arch_setup.sh

--- arch_setup.sh
+++ arch_setup.sh
@@ -135,6 +135,121 @@ fi
 
 ######################################################
 ######################################################
+# ZSH CONFIG SECTION
+######################################################
+######################################################
+read -p "Do you want to set zsh as default shell? ([y]es/[N]o): " setZsh
+if [[ $setZsh == "y" ]]; then
+    chsh -s "$(which zsh)"
+    echo -e "\e[32mZsh set as default shell successfully!!\e[0m"
+else
+    echo -e "\e[33mSkipping setting zsh as default shell...\e[0m"
+fi
+
+read -p "Do you want to install oh-my-zsh? ([y]es/[N]o): " installOhMyZsh
+if [[ $installOhMyZsh == "y" ]]; then
+    if [ ! -d "$HOME/.oh-my-zsh" ]; then
+        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        echo -e "\e[32mOh-my-zsh installed successfully!!\e[0m"
+    else
+        echo -e "\e[33mOh-my-zsh is already installed. Skipping installation...\e[0m"
+    fi
+else
+    echo -e "\e[33mSkipping oh-my-zsh installation...\e[0m"
+fi
+
+declare -A zshPlugins=(
+    ["zsh-syntax-highlighting"]="https://github.com/zsh-users/zsh-syntax-highlighting.git"
+    ["zsh-autocomplete"]="https://github.com/marlonrichert/zsh-autocomplete"
+    ["zsh-autosuggestions"]="https://github.com/zsh-users/zsh-autosuggestions"
+)
+
+echo -e "$border3$border3"
+echo ">>> (2) Zsh plugins:"
+for plugin in "${!zshPlugins[@]}"; do
+    echo "- $plugin"
+done
+echo -e "$border2"
+
+read -p "Do you want to install these zsh plugins? ([y]es/[N]o): " installZshPlugins
+if [[ $installZshPlugins == "y" ]]; then
+    for plugin in "${!zshPlugins[@]}"; do
+        pluginDir="$HOME/.oh-my-zsh/custom/plugins/$plugin"
+        if [ ! -d "$pluginDir" ]; then
+            git clone "${zshPlugins[$plugin]}" "$pluginDir"
+        else
+            echo "Skipping cloning $plugin as it already exists."
+        fi
+    done
+    echo -e "\e[32mZsh plugins installed successfully!!\e[0m"
+else
+    echo -e "\e[33mSkipping zsh plugins installation...\e[0m"
+fi
+
+linuxConfigDir="$scriptRootDir/linux_config"
+windowsConfigDir="$scriptRootDir/windows_config"
+read -p "Do you want to copy the .zshrc & .vimrc files? ([Y]es/[n]o): " copyConfigFiles
+if [[ $copyConfigFiles == "n" ]]; then
+    echo -e "\e[33mSkipping copying .zshrc and .vimrc files...\e[0m"
+else
+    cp "$linuxConfigDir/.zshrc" "$HOME/.zshrc"
+    dos2unix "$HOME/.zshrc"
+
+    # check if ~/miniconda3/bin/conda exists
+    if [ -f "$HOME/miniconda3/bin/conda" ]; then
+        ~/miniconda3/bin/conda init zsh
+    fi
+
+
+    cp "$windowsConfigDir/.vimrc" "$HOME/.vimrc"
+    dos2unix "$HOME/.vimrc"
+    echo -e "\e[32m.zshrc and .vimrc files copied successfully!!\e[0m"
+fi
+
+
+######################################################
+######################################################
+# CUSTOM CONFIG SECTION
+######################################################
+######################################################
+read -p "Do you want to setup custom Yazi config? ([Y]es/[n]o): " setupYazi
+if [[ $setupYazi == "n" ]]; then
+    echo -e "\e[33mSkipping Yazi config setup...\e[0m"
+else
+    yaziConfigDir="$HOME/.config/yazi"
+    if [ ! -d "$yaziConfigDir" ]; then
+        mkdir -p "$yaziConfigDir"
+    fi
+    customYaziConfig="$scriptRootDir/yazi_config"
+    # copy everything from yazi_config to ~/.config/yazi recursively while maintaining the directory structure
+    cp -r "$customYaziConfig/." "$yaziConfigDir"
+    echo -e "\e[32mYazi config setup successfully!!\e[0m"
+fi
+
+read -p "Do you want to setup custom fastfetch config? ([Y]es/[n]o): " setupFastfetch
+if [[ $setupFastfetch == "n" ]]; then
+    echo -e "\e[33mSkipping Fastfetch config setup...\e[0m"
+else
+    fromFastfetch="$scriptRootDir/windows_config/fastfetch_custom.jsonc"
+    toFastfetch="/usr/share/fastfetch/presets/custom.jsonc"
+    sudo cp "$fromFastfetch" "$toFastfetch"
+    echo -e "\e[32mFastfetch config setup successfully!!\e[0m"
+fi
+
+read -p "Do you want to setup custom bat config? ([Y]es/[n]o): " setupBat
+if [[ $setupBat == "n" ]]; then
+    echo -e "\e[33mSkipping Bat config setup...\e[0m"
+else
+    fromBat="$scriptRootDir/windows_config/.bat_config"
+    toBat="$HOME/.config/bat/config"
+    mkdir -p "$HOME/.config/bat" && cp "$fromBat" "$toBat"
+    echo -e "\e[32mBat config setup successfully!!\e[0m"
+fi
+
+
+
+######################################################
+######################################################
 # BYE BYE SECTION
 ######################################################
 ######################################################
