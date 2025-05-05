7a0c8f61adb7c7d33be3921d9c62b82037a29648
Prajwal-Prathiksh <prajwal.prathiksh@gmail.com>
2025-05-05 07:01:45 +0000

    Minor updates

--- windows_config/Advanced.Microsoft.PowerShell_profile.ps1
+++ windows_config/Advanced.Microsoft.PowerShell_profile.ps1
@@ -1019,11 +1019,6 @@ $env:FZF_DEFAULT_OPTS = @"
 --bind ctrl-h:preview-bottom
 --bind alt-z:toggle-preview-wrap
 --bind ctrl-e:toggle-preview
---color=fg:#908caa,bg:#191724,hl:#ebbcba
---color=fg+:#e0def4,bg+:#26233a,hl+:#ebbcba
---color=border:#403d52,header:#31748f,gutter:#191724
---color=spinner:#f6c177,info:#9ccfd8
---color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa
 "@
 
 function _fzf_open_path {
