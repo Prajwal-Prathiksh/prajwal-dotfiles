a80190808358a6cdc5d0cff8e0cc8c55ecb1f7d0
Prajwal-Prathiksh <prajwal.prathiksh@gmail.com>
2025-09-19 13:38:27 -0500

    Refactor nowplaying.sh to improve media status checks and streamline song information retrieval

--- linux_config/omarchy_configs/hypr/scripts/nowplaying.sh
+++ linux_config/omarchy_configs/hypr/scripts/nowplaying.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+# Check if media is paused
+if [ "$(playerctl status 2>/dev/null)" = "Paused" ]; then
+    exit 0
+fi
+
 player_name=$(playerctl metadata --format '{{playerName}}' | grep -iE 'spotify|chromium|edge')
 
 if echo "$player_name" | grep -iq 'spotify'; then
     icon=""
+    song_info="$(playerctl metadata --format "{{artist}} - {{title}}")"
 elif echo "$player_name" | grep -iqE 'chrome|chromium|brave|vivaldi|microsoft-edge|google-chrome'; then
     icon=""
+    song_info="$(playerctl metadata --format "{{title}}")"
 else
     icon=""
+    song_info=""
 fi
 
 
-song_info="$(playerctl metadata --format "{{artist}} - {{title}}")"
 song_info="${icon}   ${song_info}"
 
-echo "$song_info" 
+echo "$song_info"
